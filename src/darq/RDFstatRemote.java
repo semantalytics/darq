0aff5864a50e3219ae6d03d538d6ba11269a646e
bquilitz <bquilitz@fe938fe8-d216-0410-b4dc-89f26e54b4b4>
2007-12-06 17:16:37 +0000

    RDFStatREmote -> create service descriptions for SPARQL endpoints

---
+++ src/darq/RDFstatRemote.java
@@ -0,0 +1,165 @@
+package darq;
+
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import com.hp.hpl.jena.graph.Node;
+import com.hp.hpl.jena.graph.Triple;
+import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.query.darq.config.schema.DOSE;
+import com.hp.hpl.jena.query.engine1.QueryEngine;
+import com.hp.hpl.jena.query.engineHTTP.HttpQuery;
+import com.hp.hpl.jena.query.engineHTTP.QueryEngineHTTP;
+import com.hp.hpl.jena.query.resultset.ResultSetMem;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
+import com.hp.hpl.jena.util.FileManager;
+import com.hp.hpl.jena.vocabulary.RDF;
+import com.hp.hpl.jena.vocabulary.RDFS;
+import com.hp.hpl.jena.vocabulary.XSD;
+
+
+public class RDFstatRemote {
+    
+    
+    
+    private static String schemaURI= "http://darq.sf.net/dose/0.1#";
+    
+    
+  
+    private void report(String endpointURL, String graph) {
+        
+    	 Model model = ModelFactory.createDefaultModel();
+         model.setNsPrefix("sd","http://darq.sf.net/dose/0.1#");
+         model.setNsPrefix("rdfs","http://www.w3.org/2000/01/rdf-schema#");
+         model.setNsPrefix("rdf","http://www.w3.org/1999/02/22-rdf-syntax-ns#");
+
+         Resource r = model.createResource();
+         
+         model.add(r,RDF.type,model.createResource("sd:Service"));
+         model.add(r,DOSE.url,model.createResource(endpointURL));
+    	
+    	QueryEngineHTTP qe_predicates = new QueryEngineHTTP(endpointURL,"SELECT DISTINCT ?p WHERE {?s ?p ?o}");
+    	qe_predicates.addDefaultGraph(graph);
+    	ResultSet rs_predicates  = qe_predicates.execSelect();
+    	ResultSetMem rsm_predicates = new ResultSetMem(rs_predicates);
+    	
+    	
+    	long totalcount=0;
+    	
+    	while (rsm_predicates.hasNext()) {
+    		QuerySolution s = rsm_predicates.nextSolution();
+    		
+    		QueryEngineHTTP qe_count = new QueryEngineHTTP(endpointURL,"SELECT count(*) WHERE {?s <"+s.getResource("p")+"> ?o}");
+    		qe_count.addDefaultGraph(graph);
+    		long count=0;
+    		ResultSet rs_count  = qe_count.execSelect();
+    		if (rs_count.hasNext()) {
+    			count= rs_count.nextSolution().getLiteral("callret-0").getLong();
+    			totalcount+=count;
+    		}
+    		
+    		if (!s.get("p").equals(RDF.type)) {
+    		
+    		QueryEngineHTTP qe_objects = new QueryEngineHTTP(endpointURL,"SELECT DISTINCT ?o WHERE {?s <"+s.getResource("p")+"> ?o}");
+    		qe_objects.addDefaultGraph(graph);
+        	ResultSet rs_objects  = qe_predicates.execSelect();
+    		ResultSetMem rsm_objects = new ResultSetMem(rs_objects);
+    		double osel = 1.0/(new Double(rsm_objects.size()));
+    		
+    		QueryEngineHTTP qe_subjects = new QueryEngineHTTP(endpointURL,"SELECT DISTINCT ?s WHERE {?s <"+s.getResource("p")+"> ?o}");
+    		qe_subjects.addDefaultGraph(graph);
+        	ResultSet rs_subjects  = qe_predicates.execSelect();
+    		ResultSetMem rsm_subjects = new ResultSetMem(rs_subjects);
+    		double ssel = 1.0/(new Double(rsm_subjects.size()));
+    	
+    		 Resource capability = model.createResource();
+             model.add(r,DOSE.capability,capability);
+             model.add(capability,DOSE.predicate,s.getResource("p"));
+             model.add(capability,DOSE.sofilter,model.createLiteral(""));
+             
+             
+             //model.add(capability,model.createProperty("sd:","triples"),model.createLiteral(new Integer(sum)));
+             model.add(capability,DOSE.triples,model.createTypedLiteral(new Long(count),XSD.integer.getURI()));
+             
+             //model.add(capability,model.createProperty("sd:","objectSelectivity"),model.createLiteral(new Double(value)));
+             model.add(capability,model.createProperty(schemaURI,"objectSelectivity"),model.createTypedLiteral(new Double(osel),XSD.xdouble.getURI()));
+//           model.add(capability,model.createProperty("sd:","objectSelectivity"),model.createLiteral(new Double(value)));
+             model.add(capability,model.createProperty(schemaURI,"subjectSelectivity"),model.createTypedLiteral(new Double(ssel),XSD.xdouble.getURI()));
+             	
+    		
+    		} else {
+    			
+    		}
+    		
+    		
+    	}
+    	
+    	
+ 
+    	model.add(r,DOSE.totalTriples,model.createTypedLiteral(new Long(totalcount)));
+        
+        
+        
+    	 model.write(System.out,"N3");
+   
+        
+  
+        
+        
+        /*line();
+        System.out.println("Number of different predicates: "+predicates.size());
+        line();
+        
+        System.out.println("Number of classes:" +classes.size());
+        line();*/
+        
+        
+       /* int classcount =0;
+        String filterstring="";
+        
+        for (RDFNode n:classes.keySet()) {
+            if (classcount!=0) filterstring+=" || "; 
+            filterstring+= "REGEX(STR(?object),'"+n.toString()+"')";
+            classcount+=classes.get(n);
+        }
+        
+        Resource capability = model.createResource(); 
+        model.add(r,DOSE.capability,capability);
+        model.add(capability,DOSE.predicate ,RDF.type);
+        model.add(capability,DOSE.sofilter,model.createTypedLiteral(filterstring));
+        model.add(capability,DOSE.triples,model.createTypedLiteral(classcount,XSD.integer.getURI()));
+*/
+        
+  
+       
+    }
+    
+   
+    
+    
+    public static void main(String[] args) {
+        
+        if (args.length!=2) {
+            System.out.println("usage: rdfstat endpointurl graph");
+            System.out.println("eg: rdfstat http://localhost/sparql mygraph");
+            System.exit(1);
+        }
+        RDFstatRemote stat = new RDFstatRemote();
+        stat.report(args[0],args[1]);
+      
+    }
+    
+    
+    
+    
+
+}
