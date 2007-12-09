f3b513e415db1e85770d4c436e7e3f26b5419c76
bquilitz <bquilitz@fe938fe8-d216-0410-b4dc-89f26e54b4b4>
2007-12-09 15:36:24 +0000

    added explain hook

--- darq/trunk/src/com/hp/hpl/jena/query/darq/util/DARQLogHook.java
+++ darq/trunk/src/com/hp/hpl/jena/query/darq/util/DARQLogHook.java
@@ -1,6 +1,7 @@
 package com.hp.hpl.jena.query.darq.util;
 
 import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.darq.engine.optimizer.planoperators.PlanOperatorBase;
 import com.hp.hpl.jena.query.engine1.PlanElement;
 
 public interface DARQLogHook {
@@ -8,6 +9,7 @@ public interface DARQLogHook {
     public void logPlan(Query query, PlanElement planElement);
     public void logPlanOptimized(Query query, PlanElement planElement);
     public void logSubquery(Query query);
+    public void logOptimizerExplain(PlanOperatorBase pob);
     
     
     
