ad431089b81033fad8b29cc759f02fa361b9d163
Musidlowski <Musidlowski@fe938fe8-d216-0410-b4dc-89f26e54b4b4>
2008-05-13 16:16:08 +0000

    Alte Implementierung entfernt

--- darq/trunk/src/com/hp/hpl/jena/query/darq/util/MapDARQLogHook.java
+++
@@ -1,7 +0,0 @@
-package com.hp.hpl.jena.query.darq.util;
-
-import com.hp.hpl.jena.query.darq.engine.optimizer.planoperators.MapPlanOperatorBase;
-
-public interface MapDARQLogHook extends DARQLogHook {
-	public void logOptimizerExplain(MapPlanOperatorBase pob);
-}
