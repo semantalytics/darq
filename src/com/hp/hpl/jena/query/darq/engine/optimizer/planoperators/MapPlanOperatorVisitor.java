2b66c7739b1cb0c912e9b1d888764151dc52c5d7
Musidlowski <Musidlowski@fe938fe8-d216-0410-b4dc-89f26e54b4b4>
2008-05-13 16:16:08 +0000

    Alte Implementierung entfernt

--- src/com/hp/hpl/jena/query/darq/engine/optimizer/planoperators/MapPlanOperatorVisitor.java
+++
@@ -1,9 +0,0 @@
-package com.hp.hpl.jena.query.darq.engine.optimizer.planoperators;
-
-public interface MapPlanOperatorVisitor extends PlanOperatorVisitor {
-
-	public void visit(MapOperatorServiceGroup op) ;
-	public void visit(MapNestedLoopJoin op) ;
-	public void visit(MapBindJoin op) ;
-	public void visit(Union op) ;
-}
