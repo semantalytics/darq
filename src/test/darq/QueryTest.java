1eb127af85e770bc1390b72d7212b6aa12e6feb8
Musidlowski <Musidlowski@fe938fe8-d216-0410-b4dc-89f26e54b4b4>
2008-05-13 17:25:24 +0000

    nochmal register Methodenaufruf geändert

--- src/test/darq/QueryTest.java
+++ src/test/darq/QueryTest.java
@@ -208,7 +208,7 @@ public class QueryTest extends TestCaseARQ
     
             String configFile = n.asNode().toString();
             
-            FedQueryEngineFactory.register(new Configuration(configFile));
+            FedQueryEngineFactory.register(new Configuration(configFile),null,0);
             
             
             QueryExecution qe = null ;
