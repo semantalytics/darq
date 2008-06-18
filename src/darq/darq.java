5411aece11c43fcdfb7e98538ebfe46a244b4299
Musidlowski <Musidlowski@fe938fe8-d216-0410-b4dc-89f26e54b4b4>
2008-06-18 08:16:40 +0000

    Implementierung Kommandozeile + Parameter

--- src/darq/darq.java
+++ src/darq/darq.java
@@ -6,6 +6,13 @@
 
 package darq;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import com.hp.hpl.jena.query.darq.engine.FedQueryEngine;
+import com.hp.hpl.jena.query.darq.engine.FedQueryEngineFactory;
+
 import arq.cmd.CmdUtils;
 
 /** A program to execute queries from the command line in ARQ mode.
@@ -21,8 +28,40 @@ public class darq
     //static protected Log logger = LogFactory.getLog( arq.class );
 
     public static void main (String [] argv)
-    {
-        query.main(argv);
+    { 
+    	String [] zeile; 
+    	String eingabe = "";
+    	if (argv.length == 0){
+			System.out.println("Welcome to DARQ!");
+		}
+    	do{
+    		if (argv.length != 0){
+    			zeile = argv;
+    		}
+    		else{
+    			System.out.println("");
+    			System.out.println("Type '--help' for help");
+    			System.out.println("or 'exit' to quit");
+
+    			BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+    			zeile = null;
+    			try {
+    				eingabe = console.readLine();
+    				zeile = eingabe.split(" ");
+    			} catch (IOException e) {
+    				// Sollte eigentlich nie passieren
+    				e.printStackTrace();
+    			}
+    		}
+    		if(!eingabe.equals("exit")){
+//    			for(String part:zeile){ //TESTAUSGABE
+//    				System.out.println(part);
+//    			}
+    			query.main(zeile);
+    			FedQueryEngineFactory.unregister();
+    			argv  = new String[0];
+    		}
+    		
+    	}while(!eingabe.equals("exit"));
     }
-        
 }
