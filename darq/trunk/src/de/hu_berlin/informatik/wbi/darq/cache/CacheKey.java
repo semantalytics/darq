e1010c831688839a0b7147f8e326bbf610011202
Musidlowski <Musidlowski@fe938fe8-d216-0410-b4dc-89f26e54b4b4>
2008-06-19 13:38:27 +0000

    added filter to Cachekey

--- darq/trunk/src/de/hu_berlin/informatik/wbi/darq/cache/CacheKey.java
+++ darq/trunk/src/de/hu_berlin/informatik/wbi/darq/cache/CacheKey.java
@@ -7,38 +7,42 @@ public class CacheKey implements Serializable {
 
 	private List<TripleStringURI> tripleStringURI;
 	private String remoteURL;
+	private String filters;
 
-	public CacheKey(List<TripleStringURI> tripleStringURI, String remoteURL) {
+	public CacheKey(List<TripleStringURI> tripleStringURI, String remoteURL, String filters) {
 		this.tripleStringURI = tripleStringURI;
 		this.remoteURL = remoteURL;
+		this.filters = filters;
 	}
 
-	public boolean equals(Object obj){
+	public boolean equals(Object obj) {
 		CacheKey cacheKey = null;
 		if (obj instanceof CacheKey) {
 			cacheKey = (CacheKey) obj;
-			if (remoteURL.equals(cacheKey.getRemoteURL()) && tripleStringURI.equals(cacheKey.getTripleStringURI())) {
+			if (remoteURL.equals(cacheKey.getRemoteURL()) && tripleStringURI.equals(cacheKey.getTripleStringURI()) && filters.equals(cacheKey.getFilters())) {
 				return true;
 			}
 		}
 		return false;
 	}
-	
+
 	public int hashCode() {
 		String s, p, o;
 		int hc;
-		hc = remoteURL.hashCode();
+		hc = remoteURL.hashCode() ^ filters.hashCode();
 		for (TripleStringURI t : tripleStringURI) {
 
 			s = t.getSubject();
-			if (s!=null)
+			if (s != null)
 				hc = hc ^ s.hashCode();
-			
+
 			p = t.getPredicate();
-			if(p!=null) hc = hc ^p.hashCode();
-			
+			if (p != null)
+				hc = hc ^ p.hashCode();
+
 			o = t.getObject();
-			if(o!=null) hc = hc ^o.hashCode();
+			if (o != null)
+				hc = hc ^ o.hashCode();
 		}
 		return hc;
 	}
@@ -49,6 +53,10 @@ public class CacheKey implements Serializable {
 
 	public String getRemoteURL() {
 		return remoteURL;
+	}
+
+	public String getFilters() {
+		return filters;
 	}
 
 }
--- darq/trunk/src/de/hu_berlin/informatik/wbi/darq/cache/Caching.java
+++ darq/trunk/src/de/hu_berlin/informatik/wbi/darq/cache/Caching.java
@@ -17,111 +17,93 @@ public class Caching {
 
 	private static Caching instance;
 	private static String configFile;
-	private static boolean  on = false;
+	private static boolean on = false;
 	CacheManager singletonManager;
 	Cache darqCache;
-	
+
 	private Caching() {
 
 		/* Cache parameters */
-		/* see http://ehcache.sourceforge.net/documentation/configuration.html for explanation */
-//		String name = "DarqResultSetCache";
-//		int maxElementsInMemory = 50; //TEST Werte testen 
-//		int maxElementsOnDisk = 0; // 0 == unlimited
-//		boolean eternal = false; // Element will expire
-//		boolean overflowToDisk = true; //write to disk after maxInMemory
-//		boolean diskPersistent = true; // TODO TEST store to disk between restarts of the VM
-//		// mit maxElementOnDisk = unlimited --> Gefährlich !! 
-//		long timeToLiveSeconds = 0; // 0 == infinity
-//		long timeToIdleSeconds = 0; // 0 == infinity
-//		int diskSpoolBufferSizeMB = 30; //Buffer before storing to disk
-//		MemoryStoreEvictionPolicy memoryStoreEvictionPolicy = MemoryStoreEvictionPolicy.LFU;
-//		String diskStorePath = System.getProperty("java.io.tempdir");			
-//		long diskExpiryThreadIntervalSeconds = 120;
-
-//		RegisteredEventListeners registeredEventListeners = null;
-//		BootstrapCacheLoader bootstrapCacheLoader = null;
-
-//		singletonManager = CacheManager.create();	
-//		darqCache = new Cache(name, maxElementsInMemory,memoryStoreEvictionPolicy, overflowToDisk,diskStorePath, eternal, timeToLiveSeconds, timeToIdleSeconds, diskPersistent,diskExpiryThreadIntervalSeconds, registeredEventListeners,bootstrapCacheLoader,maxElementsOnDisk,diskSpoolBufferSizeMB);
-//		singletonManager.addCache("DarqResultSetCache");
-
+		/*
+		 * see http://ehcache.sourceforge.net/documentation/configuration.html
+		 * for explanation
+		 */
+		// String name = "DarqResultSetCache";
+		// int maxElementsInMemory = 50; //TEST Werte testen
+		// int maxElementsOnDisk = 0; // 0 == unlimited
+		// boolean eternal = false; // Element will expire
+		// boolean overflowToDisk = true; //write to disk after maxInMemory
+		// boolean diskPersistent = true; // TODO TEST store to disk between
+		// restarts of the VM
+		// // mit maxElementOnDisk = unlimited --> Gefährlich !!
+		// long timeToLiveSeconds = 0; // 0 == infinity
+		// long timeToIdleSeconds = 0; // 0 == infinity
+		// int diskSpoolBufferSizeMB = 30; //Buffer before storing to disk
+		// MemoryStoreEvictionPolicy memoryStoreEvictionPolicy =
+		// MemoryStoreEvictionPolicy.LFU;
+		// String diskStorePath = System.getProperty("java.io.tempdir");
+		// long diskExpiryThreadIntervalSeconds = 120;
+		// RegisteredEventListeners registeredEventListeners = null;
+		// BootstrapCacheLoader bootstrapCacheLoader = null;
+		// singletonManager = CacheManager.create();
+		// darqCache = new Cache(name,
+		// maxElementsInMemory,memoryStoreEvictionPolicy,
+		// overflowToDisk,diskStorePath, eternal, timeToLiveSeconds,
+		// timeToIdleSeconds, diskPersistent,diskExpiryThreadIntervalSeconds,
+		// registeredEventListeners,bootstrapCacheLoader,maxElementsOnDisk,diskSpoolBufferSizeMB);
+		// singletonManager.addCache("DarqResultSetCache");
 
 		/* create() use log, constructor does not */
-//		System.out.println(configFile);//TESTAUSGABE
 		singletonManager = CacheManager.create(configFile);
-//		for(String name : singletonManager.getCacheNames()){ // TESTAUSGABE
-//		System.out.println(name);
-//		}			
 		darqCache = singletonManager.getCache("DarqResultSetCache");
 	}
 
-
 	public synchronized static Caching getInstance(String confFile) {
 		configFile = confFile;
-//		System.out.println("Caching Konstruktor");//TESTAUSGABE
 		if (instance == null) {
 			instance = new Caching();
 		}
 		on = true;
 		return instance;
 	}
-	//FRAGE Was ist Referenz für ResultSet? Nach was kann ich suchen?
-	/* 1. Der RS muss die Query beantwortet haben
-	 * 2. Es muss unabhängig von Variablennamen sein*/
-//	@Deprecated
-//	public void addElement(CacheKey cacheKey, List<QuerySolution> qs){
-//		System.out.println("Caching Add Element");
-//		cacheKey.output();
-//		Element el = new Element(cacheKey, qs);
-//		System.out.println("serializable: " + el.isSerializable());
-//		darqCache.put(el);	
-//		darqCache.flush();
-//	}
 
-	public void addElement(ServiceGroup serviceGroup, List<Binding> bindings){
+	public void addElement(ServiceGroup serviceGroup, List<Binding> bindings) {
 		CacheKey key;
 
+		key = getKey(serviceGroup);
+		Element el = new Element(key, bindings);
+		darqCache.put(el);
 		/*
-		 * add element only when there is no filter because can not meassure
-		 * which filter is less or more restrictive. Just in case filter in query is more 
-		 * restrictive than filter of the cached results, the cache make sense,
-		 * otherway ask remoteservice
+		 * write to disk, QuerySolution (and all Elements within) has to be
+		 * serializable
 		 */
-		if (serviceGroup.getFilters().isEmpty()) {
-
-			key = getKey(serviceGroup);		
-			Element el = new Element(key, bindings);			
-			darqCache.put(el);
-			/* write to disk, QuerySolution (and all Elements within) has to be serializable */ 
-//			System.out.println("serializable: " + el.isSerializable()); //TESTAUSGABE
-//			darqCache.flush();
-		}
+		// System.out.println("serializable: " + el.isSerializable());
+		// //TESTAUSGABE
+		// darqCache.flush();
 	}
-	
+
 	/* get key for element, get value for key */
-	public List<Binding> getElement(ServiceGroup serviceGroup){
+	public List<Binding> getElement(ServiceGroup serviceGroup) {
 		List<Binding> bindings = new ArrayList<Binding>();
 		Element element;
 		CacheKey key = getKey(serviceGroup);
-//		System.out.println("Key: "+key.hashCode()); //TESTAUSGABE
 		element = darqCache.get(key);
-		if (element != null){
-			bindings = (List<Binding>) element.getObjectValue();	
+		if (element != null) {
+			bindings = (List<Binding>) element.getObjectValue();
 		}
 		return bindings;
-		
+
 	}
-	
-	private CacheKey getKey(ServiceGroup serviceGroup){
+
+	private CacheKey getKey(ServiceGroup serviceGroup) {
 		CacheKey key;
-		Node s,p,o;
-		String subj,pred,obj;
+		Node s, p, o;
+		String subj, pred, obj, filters;
 		subj = null;
 		pred = null;
 		obj = null;
 		List<TripleStringURI> tripleList = new ArrayList<TripleStringURI>();
-		
+
 		List<Triple> triples = serviceGroup.getTriples();
 		for (Triple triple : triples) {
 			s = triple.getSubject();
@@ -136,37 +118,37 @@ public class Caching {
 			TripleStringURI tripleURI = new TripleStringURI(subj, pred, obj);
 			tripleList.add(tripleURI);
 		}
-		key = new CacheKey(tripleList, serviceGroup.getService().getUrl());
+		key = new CacheKey(tripleList, serviceGroup.getService().getUrl(), serviceGroup.getFilters().toString());
 		return key;
 	}
-	
-	public void output(){
+
+	public void output() {
 		System.out.println("Cache Memory Size: " + darqCache.getMemoryStoreSize());
 		System.out.println("Cache Disk Size: " + darqCache.getDiskStoreSize());
-		for(Object key : darqCache.getKeys()){
+		for (Object key : darqCache.getKeys()) {
 			CacheKey cacheKey = (CacheKey) key;
 			System.out.println("Key:" + key.hashCode());
-			Element element= darqCache.get(cacheKey);
+			Element element = darqCache.get(cacheKey);
 			List<Binding> werte = (List<Binding>) element.getValue();
-			for(Binding sol : werte){
+			for (Binding sol : werte) {
 				System.out.println("   Wert" + sol.toString());
 			}
 		}
-		
 	}
-	public void removeCache(){
+
+	public void removeCache() {
 		/* write to disk */
-//		darqCache.flush(); //FRAGE sinnvoll?
+		// darqCache.flush();
 		singletonManager.removalAll();
-		singletonManager.shutdown();		
+		singletonManager.shutdown();
 	}
-	
-	public boolean isEnabled(){
+
+	public boolean isEnabled() {
 		return on;
 	}
 
 	public String getConfigFile() {
 		return configFile;
 	}
-	
+
 }
