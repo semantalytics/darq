a50fdbbe90bf375c30ff21351b21901dace62a80
Musidlowski <Musidlowski@fe938fe8-d216-0410-b4dc-89f26e54b4b4>
2008-06-22 15:59:48 +0000

    Extension to cache that different variables in Query and CacheResult are no problems anymore. Filtering is also available now

--- src/de/hu_berlin/informatik/wbi/darq/cache/CacheKey.java
+++ src/de/hu_berlin/informatik/wbi/darq/cache/CacheKey.java
@@ -3,23 +3,27 @@ package de.hu_berlin.informatik.wbi.darq.cache;
 import java.io.Serializable;
 import java.util.List;
 
+import com.hp.hpl.jena.query.darq.core.ServiceGroup;
+
 public class CacheKey implements Serializable {
 
 	private List<TripleStringURI> tripleStringURI;
 	private String remoteURL;
 	private String filters;
+	private ServiceGroup serviceGroup;
 
-	public CacheKey(List<TripleStringURI> tripleStringURI, String remoteURL, String filters) {
+	public CacheKey(List<TripleStringURI> tripleStringURI, String remoteURL, String filters, ServiceGroup serviceGroup) {
 		this.tripleStringURI = tripleStringURI;
 		this.remoteURL = remoteURL;
 		this.filters = filters;
+		this.serviceGroup = serviceGroup;
 	}
 
 	public boolean equals(Object obj) {
 		CacheKey cacheKey = null;
 		if (obj instanceof CacheKey) {
 			cacheKey = (CacheKey) obj;
-			if (remoteURL.equals(cacheKey.getRemoteURL()) && tripleStringURI.equals(cacheKey.getTripleStringURI()) && filters.equals(cacheKey.getFilters())) {
+			if (remoteURL.equals(cacheKey.getRemoteURL()) && tripleStringURI.equals(cacheKey.getTripleStringURI())&& filters.equals(cacheKey.getFilters())) {
 				return true;
 			}
 		}
@@ -57,6 +61,10 @@ public class CacheKey implements Serializable {
 
 	public String getFilters() {
 		return filters;
+	}
+
+	public ServiceGroup getServiceGroup() {
+		return serviceGroup;
 	}
 
 }
--- src/de/hu_berlin/informatik/wbi/darq/cache/Caching.java
+++ src/de/hu_berlin/informatik/wbi/darq/cache/Caching.java
@@ -1,7 +1,12 @@
 package de.hu_berlin.informatik.wbi.darq.cache;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import net.sf.ehcache.Cache;
 import net.sf.ehcache.CacheManager;
@@ -9,9 +14,11 @@ import net.sf.ehcache.Element;
 
 import com.hp.hpl.jena.graph.Node;
 import com.hp.hpl.jena.graph.Triple;
-import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.query.core.Var;
 import com.hp.hpl.jena.query.darq.core.ServiceGroup;
 import com.hp.hpl.jena.query.engine.Binding;
+import com.hp.hpl.jena.query.engine.BindingMap;
+import com.hp.hpl.jena.query.expr.Expr;
 
 public class Caching {
 
@@ -52,7 +59,6 @@ public class Caching {
 		// timeToIdleSeconds, diskPersistent,diskExpiryThreadIntervalSeconds,
 		// registeredEventListeners,bootstrapCacheLoader,maxElementsOnDisk,diskSpoolBufferSizeMB);
 		// singletonManager.addCache("DarqResultSetCache");
-
 		/* create() use log, constructor does not */
 		singletonManager = CacheManager.create(configFile);
 		darqCache = singletonManager.getCache("DarqResultSetCache");
@@ -69,31 +75,92 @@ public class Caching {
 
 	public void addElement(ServiceGroup serviceGroup, List<Binding> bindings) {
 		CacheKey key;
-		if (serviceGroup.getFilters().isEmpty()){
-			key = getKey(serviceGroup);
-			Element el = new Element(key, bindings);
-			darqCache.put(el);
-			/*
-			 * write to disk, QuerySolution (and all Elements within) has to be
-			 * serializable
-			 */
-			// System.out.println("serializable: " + el.isSerializable());
-			// //TESTAUSGABE
-			// darqCache.flush();
-		}
+		// if (serviceGroup.getFilters().isEmpty()){
+		key = getKey(serviceGroup);
+		Element el = new Element(key, bindings);
+		darqCache.put(el);
+		/*
+		 * write to disk, QuerySolution (and all Elements within) has to be
+		 * serializable
+		 */
+		// System.out.println("serializable: " + el.isSerializable());
+		// //TESTAUSGABE
+		// darqCache.flush();
+		// }
 	}
 
 	/* get key for element, get value for key */
 	public List<Binding> getElement(ServiceGroup serviceGroup) {
 		List<Binding> bindings = new ArrayList<Binding>();
+		List<Binding> newBindings = new ArrayList<Binding>();
 		Element element;
-		CacheKey key = getKey(serviceGroup);
+		CacheKey key;
+
+		key = getKey(serviceGroup);
 		element = darqCache.get(key);
+		/*
+		 * result is filtered anyway, so you can look if an unfiltered result is
+		 * in Cache
+		 */
+		if (!serviceGroup.getFilters().isEmpty() && element == null) {
+			ServiceGroup sgWithoutFilter = serviceGroup.clone();
+			List<Expr> filters = new ArrayList<Expr>();
+			sgWithoutFilter.setFilters(filters);
+			key = getKey(sgWithoutFilter);
+			element = darqCache.get(key);
+		}
+
 		if (element != null) {
 			bindings = (List<Binding>) element.getObjectValue();
+			newBindings.addAll(changeVariables(bindings, serviceGroup, element));
 		}
-		return bindings;
+		return newBindings;
+	}
 
+	private List<Binding> changeVariables(List<Binding> cacheBindings, ServiceGroup serviceGroup, Element element) {
+
+		Map<Node, Node> mappingVariables = new HashMap<Node,Node>();
+		Var newVariable, cacheVariable;
+		List<Binding> newBindings = new ArrayList<Binding>();
+
+		/* get Triples from cache and query */
+		List<Triple> cacheTriples = ((CacheKey) element.getObjectKey()).getServiceGroup().getTriples();
+		List<Triple> newTriples = serviceGroup.getTriples();
+
+		
+		/* gets mapping for variables*/
+		if (!newTriples.equals(cacheTriples)) {
+			for (Triple newTriple : newTriples) {
+				for (Triple cacheTriple : cacheTriples) {
+					
+					mappingVariables.putAll(equalTriple(newTriple, cacheTriple));
+				}
+			}
+			/* changes variables in bindings from cache to variables from query */ 
+			if (!mappingVariables.isEmpty()) {
+				for (Binding cacheBinding : cacheBindings) {
+					// System.out.println("Cache: " + cacheBinding.toString());//TESTAUSGABE
+					BindingMap bm = new BindingMap();
+					for (Iterator iter = cacheBinding.vars(); iter.hasNext();) {
+						cacheVariable = (Var) iter.next(); 
+						// System.out.println("CacheVariable: " + cacheVariable);//TESTAUSGABE
+						Node bindingValue = (Node) cacheBinding.get(cacheVariable); 
+						// System.out.println(" Wert: " + bindingValue);//TESTAUSGABE
+						newVariable = (Var) mappingVariables.get(cacheVariable);
+						// System.out.println("NeuVariable: " + newVariable);//TESTAUSGABE
+						if (bindingValue != null) {
+							bm.add(newVariable, bindingValue); 
+							// System.out.println(newVariable + " = " + bindingValue); //TESTAUSGABE
+						}
+					}
+					newBindings.add(bm);
+				}
+			}
+		}
+		else{
+			newBindings = cacheBindings;
+		}
+		return newBindings;
 	}
 
 	private CacheKey getKey(ServiceGroup serviceGroup) {
@@ -119,8 +186,50 @@ public class Caching {
 			TripleStringURI tripleURI = new TripleStringURI(subj, pred, obj);
 			tripleList.add(tripleURI);
 		}
-		key = new CacheKey(tripleList, serviceGroup.getService().getUrl(), serviceGroup.getFilters().toString());
+		key = new CacheKey(tripleList, serviceGroup.getService().getUrl(), serviceGroup.getFilters().toString(), serviceGroup);
 		return key;
+	}
+
+	private Map<Node, Node> equalTriple(Triple newTriple, Triple cacheTriple) {
+		HashMap<Node, Node> mappingVariables = new HashMap<Node, Node>();
+		Boolean subj, pred, obj;
+		Node s, p, o, cacheS, cacheP, cacheO;
+		subj = false;
+		pred = false;
+		obj = false;
+
+		s = newTriple.getSubject();
+		cacheS = cacheTriple.getSubject();
+		if (s.isConcrete()) {
+			subj = s.equals(cacheS);
+		} else {
+			subj = true;
+			mappingVariables.put(cacheS, s);
+		}
+
+		p = newTriple.getPredicate();
+		cacheP = cacheTriple.getPredicate();
+		if (p.isConcrete()) {
+			pred = p.equals(cacheP);
+		} else {
+			pred = true;
+			mappingVariables.put(cacheP, p);
+		}
+
+		o = newTriple.getObject();
+		cacheO = cacheTriple.getObject();
+		if (o.isConcrete()) {
+			obj = o.equals(cacheO);
+		} else {
+			obj = true;
+			mappingVariables.put(cacheO, o);
+		}
+
+		if (subj && pred && obj) {
+			return mappingVariables;
+		} else {
+			return new HashMap<Node,Node>();
+		}
 	}
 
 	public void output() {
