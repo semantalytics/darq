901e81a8b99a89bc0a87d2ab11ef2f9d9c7cdd44
Musidlowski <Musidlowski@fe938fe8-d216-0410-b4dc-89f26e54b4b4>
2008-07-23 13:38:55 +0000

    added cache support for stringconcat/-split

--- darq/trunk/src/com/hp/hpl/jena/query/darq/engine/compiler/iterators/DarqQueryIterator.java
+++ darq/trunk/src/com/hp/hpl/jena/query/darq/engine/compiler/iterators/DarqQueryIterator.java
@@ -89,8 +89,10 @@ public abstract class DarqQueryIterator extends QueryIterRepeatApply {
 		Query query = remoteQuery.getNextQuery();
 		List<Binding> newBindings = new ArrayList<Binding>();
 		List<Binding> BindingsOriginal = new ArrayList<Binding>();
+		
 		List<Binding> cacheResult = new ArrayList<Binding>();
 		List<Binding> cacheResultTransformed  = new ArrayList<Binding>();
+		
 		HashMap<String ,Double> multiplier = new HashMap<String ,Double>();
 		Boolean transform = false;
 		
@@ -102,20 +104,17 @@ public abstract class DarqQueryIterator extends QueryIterRepeatApply {
 
 				/* ask cache */
 				cacheResult = cache.getElement(serviceGroup);
+				
+				/* found in Cache */
 				if (!cacheResult.isEmpty()) {
-					/* found in Cache */
-					
 					System.out.println("[DarqQueryIterator] Cache Hit");
+					int index = 0;
+					cacheResultTransformed.addAll(cacheResult);
 					
 					/* transformation multiply */
 					if (serviceGroup instanceof MultiplyServiceGroup) {
 						multiplier = getMultiplier();
-						int index = 0;
-
-//						if (!multiplier.isEmpty()){
-						cacheResultTransformed.addAll(cacheResult);//notwendig?
-
-
+						
 						/* go through bindings */
 						for (Binding cacheBinding : cacheResult) {
 							/* get index of current binding, required for delete */
@@ -165,6 +164,117 @@ public abstract class DarqQueryIterator extends QueryIterRepeatApply {
 
 						newBindings.addAll(cacheResultTransformed);
 					}
+					
+					/* transform StringConcat */
+					else if (serviceGroup instanceof StringConcatServiceGroup) {
+						StringConcatServiceGroup scSG = (StringConcatServiceGroup) serviceGroup;
+						HashMap<Integer, String> variableValues= new HashMap<Integer, String>();
+						/* go through bindings */
+						for (Binding cacheBinding : cacheResult) {
+							
+							/* get index of current binding, required for delete */
+							index = cacheResult.indexOf(cacheBinding);
+							BindingMap bm = new BindingMap(binding);
+							
+							String scObjValue="";
+							String originalVariable;
+							Integer countVariables=0;
+							
+							/* go through variables of binding */
+							for (Iterator variableIter = cacheBinding.vars(); variableIter.hasNext();) {
+								Var var = (Var) variableIter.next();
+								String varName = var.getName();
+								Node obj = cacheBinding.get(Var.alloc(varName));
+								
+								/* concat */
+								if(scSG.isConcat()&& !scSG.getTripleInHead()){
+									HashMap<String, List<String>> concatVariables = getStringConcatVariables();
+//									List<String> conVariables = concatVariables.get("concat"); 
+									HashMap<Integer, String> conVariables = scSG.getVariablesOrderedByRule(); 
+									
+									Integer realIndex =0;
+									if (!conVariables.isEmpty()) {
+										if (conVariables.values().contains(varName)) {
+											transform = true;
+//											Literal objLiteral = (Literal) obj; //Node_Literal
+											String objLiteral = (String) obj.getLiteralValue();
+											for(Integer variableIndex : conVariables.keySet()){
+												if(conVariables.get(variableIndex).equals(varName)){
+													realIndex = variableIndex;
+												}
+											}
+											variableValues.put(realIndex, objLiteral);								
+											countVariables++;
+										}
+										
+										if (countVariables == conVariables.size()) {
+											for(Integer variableIndex = 0 ;  variableIndex < variableValues.size();variableIndex++ ){
+												if (scObjValue.equals("")){
+													scObjValue = variableValues.get(variableIndex);
+												}
+												else{
+													scObjValue = scObjValue + " "+ variableValues.get(variableIndex);	
+												}
+											}
+											originalVariable = concatVariables.get("original").iterator().next();
+											System.out.println("[DarqQueryIterator] new Binding: " + originalVariable + " = " + scObjValue);
+											Model model = ModelFactory.createDefaultModel();
+											Literal objNode = model.createTypedLiteral(scObjValue);
+											bm.add(Var.alloc(originalVariable), objNode.asNode());
+										}
+
+									}	
+								}
+								else if(scSG.isConcat() && scSG.getTripleInHead()){
+									/* no transformation, keep this binding*/
+									/*ist der fall, wenn head bei StringConcat abgefragt wird*/
+									newBindings.addAll(cacheResult);
+								}
+								
+								else if(!scSG.isConcat() && scSG.getTripleInHead()){
+									HashMap<String, List<String>> splitVariables = getStringSplitVariables();
+									List<String> spVariables = splitVariables.get("split");
+									Integer variableIndex=0;
+									
+									if (!spVariables.isEmpty()) {
+										if (spVariables.contains(varName)) { 
+											transform = true;
+//											Literal objLiteral = (Literal) obj; /*hier steigt er aus */ 
+											scObjValue = (String) obj.getLiteralValue();
+											Pattern p = Pattern.compile(" ");
+											String[] scObjValues = p.split(scObjValue);
+											Model model = ModelFactory.createDefaultModel();
+											Literal objNode;
+											variableIndex= 0;
+											HashMap<Integer, String>originalVariables = scSG.getVariablesOrderedByRule();											
+											for (String newValue : scObjValues) {
+												objNode = model.createTypedLiteral(newValue);
+												originalVariable = originalVariables.get(variableIndex);
+												System.out.println("[DarqQueryIterator] new Binding: "+ originalVariable + " = "+ objNode.toString()+", index "+index);//TESTAUSGABE
+												bm.add(Var.alloc(originalVariable), objNode.asNode());
+												variableIndex++;
+											}
+										}
+										else if (obj != null) { /* scSG only with head triple (concat) and scSG with body triples (split) */
+											bm.add(Var.alloc(varName), obj); //wahrscheinlich nicht notwendig, da ja vorher an cacheResulTransformed übergeben
+										}
+									}
+									else if (obj != null) { /* scSG only with head triple (concat) and scSG with body triples (split) */
+										bm.add(Var.alloc(varName), obj);
+									}
+								}
+								else{
+									newBindings.addAll(cacheResult);	
+								}
+								
+							}
+							cacheResultTransformed.remove(index);
+							cacheResultTransformed.add(index, bm);
+						}
+						if (transform){
+							newBindings.addAll(cacheResultTransformed);
+						}
+					}
 					else{
 						/* no transformation */
 						newBindings.addAll(cacheResult);
@@ -192,7 +302,8 @@ public abstract class DarqQueryIterator extends QueryIterRepeatApply {
 					BindingMap bm = new BindingMap(binding);
 					BindingMap bmOriginal = new BindingMap(binding);
 					QuerySolution sol = remoteResults.nextSolution();
-
+					HashMap<Integer, String> variableValues= new HashMap<Integer, String>();
+					
 					for (Iterator solVars = sol.varNames(); solVars.hasNext();) {
 						String varName = (String) solVars.next();
 
@@ -221,22 +332,33 @@ public abstract class DarqQueryIterator extends QueryIterRepeatApply {
 							if (scSG.isConcat() && !scSG.getTripleInHead()) {
 								/* concat */
 								HashMap<String, List<String>> concatVariables = getStringConcatVariables();
-								List<String> conVariables = concatVariables.get("concat");
+								HashMap<Integer, String> conVariables = scSG.getVariablesOrderedByRule(); 
+								
+								//concatVariables.get("concat");
+								Integer realIndex =0;
 								if (!conVariables.isEmpty()) {
-									if (conVariables.contains(varName)) {
+									if (conVariables.values().contains(varName)) {
 										bmOriginal.add(Var.alloc(varName), obj.asNode());
 										transform = true;
 										Literal objLiteral = (Literal) obj;
-
-										if (scObjValue.equals("")) {
-											scObjValue = objLiteral.getString();
-										} else {
-											scObjValue = scObjValue + " " + objLiteral.getString();
+										for(Integer index : conVariables.keySet()){
+											if(conVariables.get(index).equals(varName)){
+												realIndex = index;
+											}
 										}
+										variableValues.put(realIndex, objLiteral.getString());								
 										countVariables++;
 									}
 									if (countVariables == conVariables.size()) {
-
+										for(Integer index =0 ;  index < variableValues.size();index++ ){
+											if (scObjValue.equals("")){
+												scObjValue = variableValues.get(index);
+											}
+											else{
+												scObjValue = scObjValue + " "+ variableValues.get(index);	
+											}
+											
+										}
 										originalVariable = concatVariables.get("original").iterator().next();
 										System.out.println("[DarqQueryIterator] new Binding: " + originalVariable + " = " + scObjValue);
 										Model model = ModelFactory.createDefaultModel();
@@ -245,15 +367,14 @@ public abstract class DarqQueryIterator extends QueryIterRepeatApply {
 									}
 								}
 							}
+							/* split */
 							else if (!scSG.isConcat() && scSG.getTripleInHead()){
 								
 								HashMap<String, List<String>> splitVariables = getStringSplitVariables();
 								List<String> spVariables = splitVariables.get("split");
-//								List<String> originalVariables = splitVariables.get("original");
 								Integer index=0;
 								
 								if (!spVariables.isEmpty()) {
-									System.out.println("[DarqQueryIterator]: split to implement");
 									if (spVariables.contains(varName)) { 
 										bmOriginal.add(Var.alloc(varName), obj.asNode());
 										transform = true;
@@ -265,38 +386,22 @@ public abstract class DarqQueryIterator extends QueryIterRepeatApply {
 										Model model = ModelFactory.createDefaultModel();
 										Literal objNode;
 										index= 0;
-										/* rückwärts durch variablen laufen, verkehrt herum eingelesen
-										 * (hoffe nicht zufall) FRAGE hängt es von Reihenfolge in Query ab?*/
-										
-										HashMap<Integer, String>originalVariables = scSG.getSplitVariables();
-										
-											
+										HashMap<Integer, String>originalVariables = scSG.getVariablesOrderedByRule();											
 										for (String newValue : scObjValues) {
 											objNode = model.createTypedLiteral(newValue);
 											originalVariable = originalVariables.get(index);
-											System.out.println("bm.add "+ originalVariable + " "+ objNode.toString()+", index "+index);
+											System.out.println("[DarqQueryIterator] new Binding: "+ originalVariable + " = "+ objNode.toString()+", index "+index);//TESTAUSGABE
 											bm.add(Var.alloc(originalVariable), objNode.asNode());
-											/*
-											 * klappt das? Reihenfolge?, Inhalt von splitvariables ?
-											 */
 											index++;
 										}
-//										countVariables++;
 									}
 									else if (obj != null) { /* scSG only with head triple (concat) and scSG with body triples (split) */
 										bm.add(Var.alloc(varName), obj.asNode());
 										bmOriginal.add(Var.alloc(varName), obj.asNode());
 									}
-//									if (index == originalVariables.size()) {
-//										originalVariable = splitVariables.get("original").get(index);
-//										System.out.println("[DarqQueryIterator] new Binding: " + originalVariable + " = " + scObjValue);
-//										Model model = ModelFactory.createDefaultModel();
-//										Literal objNode = model.createTypedLiteral(scObjValue);
-//										bm.add(Var.alloc(originalVariable), objNode.asNode());
-//									}
 								}
 							}
-							else if (obj != null) { /* scSG only with head triple (concat) and scSG with body triples (split) */
+							else if (obj != null) { /* scSG only with head triple (concat) */
 								bm.add(Var.alloc(varName), obj.asNode());
 								bmOriginal.add(Var.alloc(varName), obj.asNode());
 							}
@@ -340,10 +445,10 @@ public abstract class DarqQueryIterator extends QueryIterRepeatApply {
 	}
 	
 	private HashMap<String,List<String>> getStringConcatVariables() {
-		List<String> scVariables = new ArrayList<String>();
+//		List<String> scVariables = new ArrayList<String>();
 		List<String> originalVariables = new ArrayList<String>();
 		HashMap<String, List<String>> concatVariables = new HashMap<String, List<String>>();
-		String concat = null;
+//		String concat = null;
 		if (serviceGroup instanceof StringConcatServiceGroup) {
 			StringConcatServiceGroup scSG = (StringConcatServiceGroup) serviceGroup;			
 			Triple originalTriple = scSG.getOriginalTriple(scSG.getTriples().iterator().next());
@@ -354,38 +459,32 @@ public abstract class DarqQueryIterator extends QueryIterRepeatApply {
 				originalVariables.add( variable);
 				concatVariables.put("original", originalVariables);
 			}
-//			URI originalPredicateUri = URI.create(originalTriple.getPredicate().getURI());
-			for (Rule rule : scSG.getPredicateRules()) {
-				if (rule.isStrincConcat()) {
-//					 rule.getPart(originalPredicateUri).isHead()
-					if (scSG.isConcat()) {
-						/* concat */
-						concat = "concat";
-						for(Triple triple : scSG.getTriples()){
-							Node tripleObject = triple.getObject();
-							if (tripleObject.isVariable()){
-								String variable = tripleObject.toString(); 
-								variable = variable.substring(1, variable.length()); 
-								scVariables.add(variable);	
-							}							
-						}	
-					} 
-				} 
-			}
+//			for (Rule rule : scSG.getPredicateRules()) {
+//				if (rule.isStrincConcat()) {
+//					if (scSG.isConcat()) {
+//						/* concat */
+//						concat = "concat";
+//						for(Triple triple : scSG.getTriples()){
+//							Node tripleObject = triple.getObject();
+//							if (tripleObject.isVariable()){
+//								String variable = tripleObject.toString(); 
+//								variable = variable.substring(1, variable.length()); 
+//								scVariables.add(variable);	
+//							}							
+//						}	
+//					} 
+//				} 
+//			}
 		}
-		concatVariables.put(concat, scVariables);
+//		concatVariables.put(concat, scVariables);
 		return concatVariables;
 	}
 	
 	private HashMap<String, List<String>> getStringSplitVariables(){
-//		HashMap<Integer,String> originalVariables = new HashMap<Integer,String>();
 		List<String> spVariables = new ArrayList<String>();
 		HashMap<String, List<String>> splitVariables = new HashMap<String, List<String>>();
 		if (serviceGroup instanceof StringConcatServiceGroup) {
 			StringConcatServiceGroup scSG = (StringConcatServiceGroup) serviceGroup;
-//			if(!scSG.isConcat()){
-//				originalVariables = scSG.getSplitVariables();
-//			}
 			/* es kann nur eins sein */
 			Triple triple  = scSG.getTriples().iterator().next();
 			if(triple.getObject().isVariable()){
@@ -394,14 +493,9 @@ public abstract class DarqQueryIterator extends QueryIterRepeatApply {
 			}
 			
 		}
-//		splitVariables.put("original", originalVariables);
 		splitVariables.put("split", spVariables);
 		return splitVariables;
 	}
-	//TODO am besten in 2 Funktionen aufteilen!
-	/*Idee: scSG mit TripleInHead=false holen und Variablen "auslesen"?
-	 * vielleicht aus Query in eigener scSG? Was gibt es da*/
-//	wie komme ich an die variablen ran? TODO  passt das?
 	
 	private HashMap<String,Double> getMultiplier() {
 		HashMap<String,Double> multiplier = new HashMap<String,Double>();
